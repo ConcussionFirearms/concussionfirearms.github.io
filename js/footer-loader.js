b25384abd96023d633267f51d7a5f223ca692bc9
ConcussionFirearms <ConcussionFirearms@outlook.com>
2025-02-03 20:43:29 -0700

    Update footer-loader.js

--- js/footer-loader.js
+++ js/footer-loader.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch('footer.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('footer-placeholder').innerHTML = data;
-        })
-        .catch(error => console.error('Error loading footer:', error));
+fetch('/footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer-placeholder').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading footer:', error));
 });
