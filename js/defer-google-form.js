16a2ffc25d30185cb98de891fca00b40b97cc2ce
ConcussionFirearms <ConcussionFirearms@outlook.com>
2025-02-08 21:06:50 -0700

    Update defer-google-form.js

--- js/defer-google-form.js
+++ js/defer-google-form.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     var iframe = document.createElement("iframe");
     iframe.src = "https://docs.google.com/forms/d/e/1FAIpQLSff7Dv-ALI5-uM27KKUbgmQpunwjrOYaVa6_Sj8EUU5LI9d8g/viewform?embedded=true";
-    iframe.width = "640";
-    iframe.height = "480";
+    iframe.width = "100%";  // Make iframe width 100% of the container
+    iframe.height = "480";  // Adjust height as needed
     iframe.frameBorder = "0";
     iframe.marginHeight = "0";
     iframe.marginWidth = "0";
